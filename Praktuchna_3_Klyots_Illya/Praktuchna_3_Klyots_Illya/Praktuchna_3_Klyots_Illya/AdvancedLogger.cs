50d6cc0cccdfc8f066fa855f3ced1ca197254a56
Illya Klyots <ilyablackbox@gmail.com>
2026-05-12 08:23:43 +0300

    feat: реалізовано методи розширення для обробки ПІБ

--- Praktuchna_3_Klyots_Illya/Praktuchna_3_Klyots_Illya/Praktuchna_3_Klyots_Illya/AdvancedLogger.cs
+++ Praktuchna_3_Klyots_Illya/Praktuchna_3_Klyots_Illya/Praktuchna_3_Klyots_Illya/AdvancedLogger.cs
@@ -46,9 +46,4 @@ namespace Praktuchna_3_Klyots_Illya
             return _logAccumulator.ToString();
         }
     }
-<<<<<<< HEAD
-    //нга
-=======
-    //у5
->>>>>>> feature/advanced-logger
 }
